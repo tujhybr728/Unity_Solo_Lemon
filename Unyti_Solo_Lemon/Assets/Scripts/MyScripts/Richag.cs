adb74c257f3bf73a9c404ad088ba6fc8d45d0fa2
Rogier <tujhybr728@mail.ru>
2022-06-05 21:42:32 +0300

    Домашнее задание 2
    Собран 1 уровень имеется коллизия.

---
+++ Unyti_Solo_Lemon/Assets/Scripts/MyScripts/Richag.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Richag : MonoBehaviour
+
+{
+    [SerializeField] GameObject Player;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+              
+    }
+
+    private void Ticker()
+    {
+
+    }
+}
